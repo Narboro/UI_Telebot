9095d83cff471ba4174224310b2aa2922b949b8f
Narboro <realkid@yandex.ru>
2025-03-21 23:46:28 +0300

    added admin panel

--- Model/AnsweringSystem.cs
+++ Model/AnsweringSystem.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
 using Message = Telegram.Bot.Types.Message;
-using System.Collections;
+using System.IO;
 
 namespace UI_Telebot.Model
 {
@@ -21,6 +16,7 @@ namespace UI_Telebot.Model
         private readonly IConfiguration _configuration;
         private int NUMBER_OF_ROWS = 5;
         private string administratorUser;
+        private string workflowStatus {  get; set; }
         Dictionary<string, string[]> booksWithIds = new Dictionary<string, string[]>();
         private string[] pickedBook { get; set; } = [];
 
@@ -40,25 +36,49 @@ namespace UI_Telebot.Model
 
         public async Task OnMessageGetAsync(Message msg, UpdateType type)
         {
-            //if (msg?.Chat?.Id.ToString() == administratorUser) //TODO admin panel
-            //{
-            //    await AdminMessages(msg, bot);
-            //}
-            if (msg.Text == "/start")
+            if (msg?.Chat?.Id.ToString() == administratorUser && msg.Text == "/admin") //TODO admin panel
+            {
+                workflowStatus = "AdminPanelActive";
+                await bot.SendMessage(msg.Chat, "Панель администратора активирована", replyMarkup: new string[][]
+                {
+                ["Выданные книги", "Просроченные книги"],
+                ["Выдать книгу", "Сдать книгу"],
+                ["Сделать копию базы"]
+                });
+                return;
+            }
+            if(msg?.Chat?.Id.ToString() == administratorUser && workflowStatus == "AdminPanelActive")
+            {
+                await AdminMessages(msg, bot);
+                return;
+            }
+            if (msg?.Chat?.Id.ToString() == administratorUser && workflowStatus == "BookIssue")
+            {
+                await BookIssue(msg, DateTime.Now.ToString("dd.MM.yyyy"));
+                await bot.SendMessage(msg.Chat, "Книга выдана" , ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                workflowStatus = "";
+                return;
+            }
+            if(msg?.Chat?.Id.ToString() == administratorUser && workflowStatus == "ReturnBook")
+            {
+                await BookIssue(msg, "");
+                await bot.SendMessage(msg.Chat, "Книга возвращена", ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                workflowStatus = "";
+                return;
+            }
+            if (msg?.Text == "/start")
             {
                 await bot.SendMessage(msg.Chat, _configuration["BotConfiguration:GreetingsMessage"] ?? "ошибка ошибка",
                         replyMarkup: new InlineKeyboardMarkup().AddButtons("Список книг"));
             }
             else
             {
-                //if (msg?.Chat?.Id.ToString() != administratorUser) await bot.SendMessage(msg.Chat, "", replyMarkup: new ReplyKeyboardRemove());
-
-                await bot.SendMessage(msg.Chat, $"Вы ввели: {msg.Text}");
+                await bot.SendMessage(msg?.Chat, $"Вы ввели: {msg?.Text}");
 
                 if (booksWithIds.ContainsKey(msg.Text ?? ""))
                 {
                     pickedBook = booksWithIds[msg.Text ?? ""];
-                    string? isAvailable = (!String.IsNullOrEmpty(pickedBook[2]) ? (pickedBook[2]).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? _configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка" : _configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка" : "");
+                    string? isAvailable = (!string.IsNullOrEmpty(pickedBook[2]) ? (pickedBook[2]).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? _configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка" : _configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка" : "");
                     if (isAvailable != "")
                     {
                         await bot.SendMessage(msg.Chat, (_configuration["BotConfiguration:BookingReject"] ?? "ошибка ошибка") + " <i><b>" + isAvailable + "</b></i>", ParseMode.Html,
@@ -113,16 +133,18 @@ namespace UI_Telebot.Model
                     messageChunk = "";
                     break;
                 }
-                string? isAvailable = (!String.IsNullOrEmpty(booksDatabase?.Rows[i]?["кому выдано"] as string) ? (booksDatabase?.Rows[i]?["кому выдано"] as string).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? $" - <i><b>{_configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка"}</b></i>" : $" - <i><b>{_configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка"}</b></i>" : "");
+
+                string? forAdmin = (msg?.Chat?.Id.ToString() == administratorUser) ? " - " + booksDatabase?.Rows[i]?["код"].ToString() : "";
+                string ? isAvailable = (!string.IsNullOrEmpty(booksDatabase?.Rows[i]?["кому выдано"] as string) ? (booksDatabase?.Rows[i]?["кому выдано"] as string).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? $" - <i><b>{_configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка"}</b></i>" : $" - <i><b>{_configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка"}</b></i>" : "");
                 if (i > 0 && i % NUMBER_OF_ROWS == 0)
                 {
                     await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html);
                     messageChunk = "";
-                    messageChunk += $"{booksDatabase?.Rows[i]["Id"]} - {booksDatabase?.Rows[i]["Название"]}{isAvailable}\n";
+                    messageChunk += $"{booksDatabase?.Rows[i]["Id"]}{forAdmin} - {booksDatabase?.Rows[i]["Название"]}{isAvailable}\n";
                 }
                 else
                 {
-                    messageChunk += $"{booksDatabase?.Rows[i]["Id"]} - {booksDatabase?.Rows[i]["Название"]}{isAvailable}\n";
+                    messageChunk += $"{booksDatabase?.Rows[i]["Id"]}{forAdmin} - {booksDatabase?.Rows[i]["Название"]}{isAvailable}\n";
                 }
             }
         }
@@ -130,7 +152,7 @@ namespace UI_Telebot.Model
         private async Task SearchBooksDB(Message msg)
         {
             var selectedBooks = from row in booksDatabase.AsEnumerable()
-                                where row.Field<string>("Название").Contains(msg!.Text, StringComparison.OrdinalIgnoreCase)
+                                where row.Field<string?>("Название").Contains(msg?.Text, StringComparison.OrdinalIgnoreCase)
                                 select row;
 
             if (selectedBooks.Count() > 0)
@@ -138,7 +160,7 @@ namespace UI_Telebot.Model
                 string messageChunk = "Результаты поиска книг:\n\n";
                 foreach (var row in selectedBooks)
                 {
-                    string? isAvailable = (!String.IsNullOrEmpty(row["кому выдано"] as string) ? (row["кому выдано"] as string).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? $" - <i><b>{_configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка"}</b></i>" : $" - <i><b>{_configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка"}</b></i>" : "");
+                    string? isAvailable = (!string.IsNullOrEmpty(row["кому выдано"] as string) ? (row["кому выдано"] as string).Contains("не выдается", StringComparison.OrdinalIgnoreCase) ? $" - <i><b>{_configuration["BotConfiguration:NonBooking"] ?? "ошибка ошибка"}</b></i>" : $" - <i><b>{_configuration["BotConfiguration:AlreadyBooked"] ?? "ошибка ошибка"}</b></i>" : "");
                     messageChunk += $"{row["Id"]} - {row["Название"]}{isAvailable}\n";
                 }
                 await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html);
@@ -153,16 +175,95 @@ namespace UI_Telebot.Model
             }
         }
 
-        //private async Task AdminMessages(Message msg, TelegramBotClient bot)
-        //{
-            //if (msg.Text == "/admin")
-            //{
-            //    await bot.SendMessage(msg.Chat, "Панель администратора активирована", replyMarkup: new string[][]
-                //{
-                //    ["Help me"],
-                //    ["Call me ☎️", "Write me ✉️"]
-                //});
-            //}
-        //}
+        private async Task BookIssue(Message msg, string action)
+        {
+            List<string>? enteredValue = msg?.Text?.Split(";").ToList();
+            if (action == "")
+            {
+                enteredValue?.Add((string)null);
+                enteredValue?.Add((string)null);
+                enteredValue?.Add((string)null);
+            } else
+            {
+                enteredValue?.Add(action);
+            }
+
+            new ExcelDbReader().WriteExcel(_configuration, enteredValue);
+            booksDatabase = new ExcelDbReader().ReadExcel(_configuration);
+        }
+
+        private async Task AdminMessages(Message msg, TelegramBotClient bot)
+        {
+            string messageChunk = "";
+            if (workflowStatus == "AdminPanelActive" && msg?.Chat?.Id.ToString() == administratorUser)
+            {
+                workflowStatus = "";
+                switch (msg.Text)
+                {
+                    case "Выданные книги":
+                        messageChunk = "Id - Название - Код - Кому выдано - Дата выдачи - Дней в выдаче\n\n<b>Выданные книги:</b>\n\n";
+                        var bookedBooks = from row in booksDatabase.AsEnumerable()
+                                          where row.Field<System.DateTime?>("дата выдачи") is not null
+                                          select row;
+
+                        messageChunk += PrepareQuery(bookedBooks);
+                        await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                        break;
+                    case "Просроченные книги":
+                        messageChunk = "Id - Название - Код - Кому выдано - Дата выдачи - Дней в выдаче\n\n<b>Просроченные книги:</b>\n\n";
+                        var expiredBooks = from row in booksDatabase.AsEnumerable()
+                                           where row.Field<double?>("дней") > 90
+                                           select row;
+
+                        messageChunk += PrepareQuery(expiredBooks);
+                        await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                        break;
+                    case "Выдать книгу":
+                        messageChunk = "Введите выдачу книги через точку с запятой в формате ниже:\n\n<b>Код ; Кому выдано ; Телефон</b>";
+                        await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                        workflowStatus = "BookIssue";
+                        break;
+                    case "Сдать книгу":
+                        messageChunk = "Введите код книги, которую хотите сдать.";
+                        await bot.SendMessage(msg.Chat, messageChunk, ParseMode.Html, replyMarkup: new ReplyKeyboardRemove());
+                        workflowStatus = "ReturnBook";
+                        break;
+                    case "Сделать копию базы":
+                        string sourceFilePath = (_configuration["BotConfiguration:LIBRARY_FILEPATH"] ?? "") + (_configuration["BotConfiguration:LIBRARY_FILENAME"] ?? "");
+                        string destinationFilePath = (_configuration["BotConfiguration:LIBRARY_FILEPATH"] ?? "") + (_configuration["BotConfiguration:LIBRARY_FILENAME"]?.Replace(".xlsx", DateTime.Now.ToString("_yyyy-MM-dd_HH-mm-ss") + ".xlsx") ?? "");
+
+                        if (File.Exists(sourceFilePath))
+                        {
+                            try
+                            {
+                                File.Copy(sourceFilePath, destinationFilePath, overwrite: true);
+                                await bot.SendMessage(msg.Chat, "Копия базы создана.", replyMarkup: new ReplyKeyboardRemove());
+                            }
+                            catch (Exception ex)
+                            {
+                                await bot.SendMessage(msg.Chat, "Ошибка!", replyMarkup: new ReplyKeyboardRemove());
+                            }
+                        }
+                        else
+                        {
+                            await bot.SendMessage(msg.Chat, "Файл базы не найден", replyMarkup: new ReplyKeyboardRemove());
+                        }
+                        break;
+                    default:
+                        await bot.SendMessage(msg.Chat, "Неизвестная команда.", replyMarkup: new ReplyKeyboardRemove());
+                        break;
+                }
+            }
+        }
+
+        private string PrepareQuery(EnumerableRowCollection<DataRow> query)
+        {
+            string? messageChunk = "";
+            foreach (var row in query)
+            {
+                messageChunk += $"{row["Id"]} - {row["Название"]} - {row["Код"]} - {row["кому выдано"]} - <i><b>{row["дата выдачи"]?.ToString()?.Substring(0, 10)}</b></i> - {row["дней"]}\n";
+            }
+            return messageChunk;
+        }
     }
 }
--- Model/ExcelDbReader.cs
+++ Model/ExcelDbReader.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Microsoft.Extensions.Configuration;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.XWPF.UserModel;
+using ICell = NPOI.SS.UserModel.ICell;
 
 namespace UI_Telebot.Model
 {
@@ -25,8 +33,66 @@ namespace UI_Telebot.Model
             con.Open();
             DataTable dt = new DataTable();
             db.Fill(dt);
-
+            con.Close();
+            con.Dispose();
             return dt;
+        }
+
+        public void WriteExcel(IConfiguration _configuration, List<string>? enteredValue)
+        {
+            configuration = _configuration;
+            String? filename = (configuration["BotConfiguration:LIBRARY_FILEPATH"] ?? "") + (configuration["BotConfiguration:LIBRARY_FILENAME"] ?? "");
+
+            try
+            {
+                using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    IWorkbook workbook = new XSSFWorkbook(fs);
+                    ISheet sheet = workbook.GetSheetAt(0); // Get the first sheet
+                    IFormulaEvaluator evaluator = workbook.GetCreationHelper().CreateFormulaEvaluator();
+
+                    for (int rw = 1; rw <= sheet.LastRowNum; rw++) // Start from 1 to skip header
+                    {
+                        IRow currentRow = sheet.GetRow(rw);
+                        if (currentRow != null)
+                        {
+                            string? code = currentRow?.GetCell(22)?.ToString();
+                            if (code == enteredValue?[0])
+                            {
+                                ICell c1 = currentRow?.GetCell(7);
+                                if (c1 == null) c1 = currentRow?.CreateCell(7);
+
+                                if (enteredValue?[3] == (string)null)
+                                {
+                                    c1?.SetCellValue(enteredValue?[3]);
+                                    c1?.SetCellType(CellType.Blank);
+                                }
+                                else
+                                {
+                                    c1?.SetCellFormula($"DateValue(\"{enteredValue?[3]}\")");
+                                    c1?.SetCellType(CellType.Formula);
+                                }
+                                evaluator.EvaluateFormulaCell(c1);
+                                ICell? c2 = currentRow?.GetCell(8);
+                                if (c2 == null) c2 = currentRow?.CreateCell(8);
+                                c2?.SetCellValue(enteredValue?[1]);
+                                ICell? c3 = currentRow?.GetCell(9);
+                                if (c3 == null) c3 = currentRow?.CreateCell(9);
+                                c3?.SetCellValue(enteredValue?[2]);
+                            }
+                        }
+                    }
+
+                    // Save the changes
+                    using (FileStream fsOut = new FileStream(filename, FileMode.Create, FileAccess.Write))
+                    {
+                        workbook.Write(fsOut);
+                    }
+                }
+            } catch(Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
